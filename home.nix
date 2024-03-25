1046cc72e34645128f72d596fede43a912927a15
kpbaks <kristoffer.pbs@gmail.com>
2024-03-25 19:58:16 +0100

    feat(home): derived generation 125, added cava

--- home.nix
+++ home.nix
@@ -330,6 +330,34 @@ in rec {
     enable = true;
   };
 
+  programs.cava = {
+    enable = true;
+    settings = {
+      general.framerate = 60;
+      general.sleep_timer = 3;
+      # input.method = "alsa";
+      output.method = "noncurses";
+      # output.method = "sdl_glsl";
+      output.alacritty_sync = 0;
+      output.orientation = "bottom";
+      smoothing.noise_reduction = 88;
+      color = {
+        background = "'#000000'";
+        # foreground = "'#FFFFFF'";
+        foreground = "magenta";
+
+        gradient = 1; # on/off
+        gradient_count = 8;
+        gradient_color_1 = "#59cc33";
+        gradient_color_2 = "#80cc33";
+        gradient_color_3 = "#a6cc33";
+        gradient_color_4 = "#cccc33";
+        gradient_color_5 = "#cca633";
+        gradient_color_6 = "#cc8033";
+      };
+    };
+  };
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
